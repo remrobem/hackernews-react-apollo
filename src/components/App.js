4c6c507c6a731864dedd555920252227c70c1395
Rob Martin <rob.martin.5758@gmail.com>
2019-05-21 21:42:38 -0400

    pagination

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import LinkList from './LinkList'
 import CreateLink from './CreateLink';
 import Header from './Header'
 import Login from './Login'
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, Redirect } from 'react-router-dom'
 import Search from './Search';
 
 class App extends Component {
@@ -17,6 +17,8 @@ class App extends Component {
             <Route exact path="/create" component={CreateLink} />
             <Route exact path="/login" component={Login} />
             <Route exact path="/search" component={Search} />
+            <Route exact path="/top" component={LinkList} />
+            <Route exact path="/new/:page" component={LinkList} />
           </Switch>
         </div>
       </div>
